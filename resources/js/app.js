e25cda8d02bef2a2f30d5f5b64f15ae5a8843c7e
Patryk Kania <p.kania@multiplay.pl>
2022-06-08 16:10:53 +0200

    frontend preparation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,13 @@
 import App from "@/App.vue";
 import { createApp } from 'vue'
-import router from '@/router';
-import api from '@/helpers/api.js';
-import pinia from '@/stores/index';
-import vuetify from "./plugins/vuetify";
+import router from '@/router/index.js';
+// import api from '@/helpers/api.js';
+import pinia from '@/stores/index.js';
+import vuetify from "./plugins/vuetify.js";
 
 const app = createApp(App)
     .use(pinia)
     .use(router)
-    .use(api)
+    // .use(api)
     .use(vuetify)
     .mount('#app');
--- resources/js/plugins/vuetify.js
+++ resources/js/plugins/vuetify.js
@@ -1,28 +1,12 @@
-import Vuetify from "vuetify";
-import colors from "vuetify/lib/util/colors";
-import "vuetify/dist/vuetify.min.css";
+// import Vuetify from "vuetify";
+// // import colors from "vuetify/lib/util/colors";
+// import "vuetify/dist/vuetify.min.css";
 import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import * as components from "vuetify/lib/components";
+import * as directives from "vuetify/lib/directives";
+import { createVuetify } from "vuetify";
 
-const options = {
-    theme: {
-        light: true,
-        themes: {
-            light: {
-                primary: colors.blue.darken2,
-                accent: colors.grey.darken3,
-                secondary: colors.amber.darken3,
-                info: colors.teal.lighten1,
-                warning: colors.amber.base,
-                error: colors.deepOrange.accent4,
-                success: colors.green.accent3
-            }
-        }
-    },
-    icons: {
-        iconfont: "mdi"
-    }
-};
+const vuetify = createVuetify({ components, directives });
 
-const vuetify = new Vuetify(options);
-
-export default new Vuetify(vuetify);
+export default vuetify;
