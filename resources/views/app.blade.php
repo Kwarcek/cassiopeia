02b7a786d27bb833884a58dc2ba06e6f3ac45782
Shift <shift@laravelshift.com>
2022-07-07 13:31:31 +0000

    Use new `@vite` Blade directive

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,10 +8,10 @@
 
     <title>Laravel</title>
 
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+    @vite('resources/css/app.css')
 </head>
 <body class="antialiased">
     <div id="app"></div>
-    <script src="{{ mix('js/app.js') }}"></script>
+    @vite('resources/js/app.js')
 </body>
 </html>
