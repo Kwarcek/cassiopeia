fdaa4b2942941a86d28878e4f953863987d16877
pk <ecollatio@gmail.com>
2022-08-19 00:41:37 +0200

    vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import App from "@/App.vue";
 import { createApp } from 'vue'
 import router from "@/router/index.js";
 import pinia from "@/stores/index.js";
-import vuetify from "./plugins/vuetify.js";
+import vuetify from "@/plugins/vuetify.js";
 
 const app = createApp(App)
     .use(pinia)
--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,3 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-// import vue from '@vitejs/plugin-vue';
-// import { resolve } from 'path';
-//
-// export default defineConfig({
-//     server: {
-//         https: false,
-//         host: 'localhost',
-//     },
-//     plugins: [
-//         laravel([
-//             'resources/css/app.css',
-//             'resources/js/app.js',
-//         ]),
-//         vue({
-//             template: {
-//                 transformAssetUrls: {
-//                     base: null,
-//                     includeAbsolute: false,
-//                 },
-//             },
-//         }),
-//     ],
-//     resolve: {
-//         alias: {
-//             "@": resolve(__dirname, "./resources/js"),
-//         }
-//     },
-// });
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
