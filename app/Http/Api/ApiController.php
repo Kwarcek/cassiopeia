84a8e92629a2fd4928eac1410b967d924143a503
pk <ecollatio@gmail.com>
2022-10-02 12:04:30 +0200

    ApiController - validate pagination

--- app/Http/Api/ApiController.php
+++ app/Http/Api/ApiController.php
@@ -3,7 +3,23 @@
 namespace App\Http\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ApiController extends Controller
 {
+    private const VALID_PER_PAGE_NUMBERS = [5, 10, 15, 20, 25, 50];
+
+    public function __construct(Request $request)
+    {
+        $this->validatePagination($request);
+    }
+
+    public function validatePagination(Request $request): void
+    {
+        $request->validate([
+           'per_page' => ['sometimes', 'numeric', Rule::in(self::VALID_PER_PAGE_NUMBERS)],
+           'page' => 'sometimes|numeric',
+        ]);
+    }
 }
